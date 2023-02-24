6522e86cb0bd19c37d40e3358c645286f7ee76f6
Ansidion <14023216@uhi.ac.uk>
2023-02-24 20:40:47 +0000

    changes

--- src/App.js
+++ src/App.js
@@ -22,16 +22,13 @@ defineCustomElements(window);
           rel="noopener noreferrer"
         >
           Learn React
-<<<<<<< HEAD
         </a>
         <ion-fab>
           <ion-fab-button>
             <ion-icon name="add"></ion-icon>
           </ion-fab-button>
         </ion-fab>
-=======
         </a> */}
->>>>>>> 23de3e0b2e69fee1c8efba365b962cfa81dd3314
       </header>
 
       <IonImg src={logo} alt="The Wisconsin State Capitol building in Madison, WI at night"></IonImg>
