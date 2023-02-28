58813b843a8f5ec9fd9b4920c7201ee08009dcf7
Ansidion <14023216@uhi.ac.uk>
2023-02-28 16:07:37 +0000

    adding in new products and changing main image

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import '@ionic/react/css/core.css';
 import { defineCustomElements } from '@ionic/pwa-elements/loader';
 import { setupIonicReact, IonImg} from '@ionic/react';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
 
 function App() {
 setupIonicReact();
@@ -14,7 +15,7 @@ defineCustomElements(window);
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
     
-    <h1>This is a test</h1>
+    <h1>Bike King Borders</h1>
         {/* <a
           className="App-link"
           href="https://reactjs.org"
@@ -31,7 +32,151 @@ defineCustomElements(window);
         </a> */}
       </header>
 
-      <IonImg src={logo} alt="The Wisconsin State Capitol building in Madison, WI at night"></IonImg>
+      <IonImg src={logo} alt="bike image"></IonImg>
+
+      <IonCard>
+        <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+        <IonCardHeader>
+          <IonCardTitle>Carera Master</IonCardTitle>
+          <IonCardSubtitle>Cost: £350</IonCardSubtitle>
+        </IonCardHeader>
+
+        <IonCardContent>
+          Get the all new carrera master bike!
+        </IonCardContent>
+        <ion-button fill="clear">Purchase</ion-button>
+      </IonCard>
+
+
+         <IonCard color="primary">
+        <IonCardHeader>
+          <IonCardTitle>Card Title</IonCardTitle>
+          <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+        </IonCardHeader>
+
+        <IonCardContent>
+          Card Content
+        </IonCardContent>
+      </IonCard>
+
+      <IonCard color="secondary">
+        <IonCardHeader>
+          <IonCardTitle>Card Title</IonCardTitle>
+          <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+        </IonCardHeader>
+
+        <IonCardContent>
+          Card Content
+        </IonCardContent>
+      </IonCard>
+
+      <IonCard color="tertiary">
+        <IonCardHeader>
+          <IonCardTitle>Card Title</IonCardTitle>
+          <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+        </IonCardHeader>
+
+        <IonCardContent>
+          Card Content
+        </IonCardContent>
+      </IonCard>
+
+      <IonCard color="success">
+        <IonCardHeader>
+          <IonCardTitle>Card Title</IonCardTitle>
+          <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+        </IonCardHeader>
+
+        <IonCardContent>
+          Card Content
+        </IonCardContent>
+      </IonCard>
+
+      <IonCard color="warning">
+        <IonCardHeader>
+          <IonCardTitle>Card Title</IonCardTitle>
+          <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+        </IonCardHeader>
+
+        <IonCardContent>
+          Card Content
+        </IonCardContent>
+      </IonCard>
+
+      <IonCard color="danger">
+        <IonCardHeader>
+          <IonCardTitle>Card Title</IonCardTitle>
+          <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+        </IonCardHeader>
+
+        <IonCardContent>
+          Card Content
+        </IonCardContent>
+      </IonCard>
+
+      <IonCard color="light">
+        <IonCardHeader>
+          <IonCardTitle>Card Title</IonCardTitle>
+          <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+        </IonCardHeader>
+
+        <IonCardContent>
+          Card Content
+        </IonCardContent>
+      </IonCard>
+
+      <IonCard color="medium">
+        <IonCardHeader>
+          <IonCardTitle>Card Title</IonCardTitle>
+          <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+        </IonCardHeader>
+
+        <IonCardContent>
+          Card Content
+        </IonCardContent>
+      </IonCard>
+
+      <IonCard color="dark">
+        <IonCardHeader>
+          <IonCardTitle>Card Title</IonCardTitle>
+          <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+        </IonCardHeader>
+
+        <IonCardContent>
+          Card Content
+        </IonCardContent>
+      </IonCard>
+
+      // Nav tabs
+      <ul class="nav nav-tabs" id="navId">
+        <li class="nav-item">
+          <a href="#tab1Id" class="nav-link active">Homepage</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="#tab2Id">Action</a>
+            <a class="dropdown-item" href="#tab3Id">Another action</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#tab4Id">Action</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a href="#tab5Id" class="nav-link">Another link</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+
+      // Tab panes
+      <div class="tab-content">
+        <div class="tab-pane fade show active" id="tab1Id" role="tabpanel"></div>
+        <div class="tab-pane fade" id="tab2Id" role="tabpanel"></div>
+        <div class="tab-pane fade" id="tab3Id" role="tabpanel"></div>
+        <div class="tab-pane fade" id="tab4Id" role="tabpanel"></div>
+        <div class="tab-pane fade" id="tab5Id" role="tabpanel"></div>
+      </div>
     </div>
 
     
