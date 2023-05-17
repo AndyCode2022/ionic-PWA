036f7343afbb020d96a3659598182239a77fc95e
ANDREW WEBSTER 14023216 <14023216@uhi.ac.uk>
2023-05-17 15:56:51 +0100

    adding google api

--- src/pages/photoGallery.js
+++ src/pages/photoGallery.js
@@ -18,6 +18,17 @@ const photoGallery = () => (
           </IonCardContent>
         </IonCard>
 
+        <iframe
+          width="600"
+          height="450"
+          style="border:0"
+          loading="lazy"
+          allowfullscreen
+          referrerpolicy="no-referrer-when-downgrade"
+          src="https://www.google.com/maps/embed/v1/place?key=API_KEY
+            &q=Space+Needle,Seattle+WA">
+        </iframe> 
+
       </IonContent>
     </IonPage>
   </>
